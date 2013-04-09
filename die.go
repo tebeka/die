4e01f14fd4dd899d8bb3a863c816801a16152c0e
Miki Tebeka <miki.tebeka@gmail.com>
2013-04-09 12:13:13 -0700

    doc

--- die.go
+++ die.go
@@ -1,4 +1,21 @@
-// Emulate Perl's "die" function
+/* 
+Emulate Perl's "die" function (http://perldoc.perl.org/functions/die.html)
+
+Example:
+
+	import (
+		. "die"
+		"os"
+	)
+
+	func main() {
+		filename := "/path/to/file"
+		file, err := os.Open(filename)
+		if err != nil {
+			Die("can't open %s - %s", filename, err)
+		}
+	}
+*/
 package die
 
 import (
